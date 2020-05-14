3dd665e514535350f92e54104ba8c4fd84a2c906
Sebastian <sebhein17@gmail.com>
2020-05-14 20:23:36 +0200

    This a big one
    Adds new footer and css for it
    Adds new burgermenu and css + js for it
    Adds two new sub-pages "Discography" and "wallpapers" (both are W.I.P)
    Adds pictures for merchandise (index) and album covers (discography)
    Adds a new merch section for desktop
    Adds background pictures for the home page (first impression and merch section)
    Removes some pictures and images that were not needed

--- js/functions.js
+++ js/functions.js
@@ -1,26 +1,14 @@
 // header
-(function() {
-
-    'use strict';
-  
-    document.querySelector('.material-design-hamburger__icon').addEventListener(
-      'click',
-      function() {      
-        var child;
-        
-        document.body.classList.toggle('background--blur');
-        this.parentNode.nextElementSibling.classList.toggle('menu--on');
-  
-        child = this.childNodes[1].classList;
-  
-        if (child.contains('material-design-hamburger__icon--to-arrow')) {
-          child.remove('material-design-hamburger__icon--to-arrow');
-          child.add('material-design-hamburger__icon--from-arrow');
-        } else {
-          child.remove('material-design-hamburger__icon--from-arrow');
-          child.add('material-design-hamburger__icon--to-arrow');
-        }
-  
-      });
-  
-  })();
+$(document).ready(function() {
+    $(document).delegate('.open', 'click', function(event) {
+        $(this).addClass('oppenned');
+        event.stopPropagation();
+    })
+    $(document).delegate('body', 'click', function(event) {
+        $('.open').removeClass('oppenned');
+    })
+    $(document).delegate('.cls', 'click', function(event) {
+        $('.open').removeClass('oppenned');
+        event.stopPropagation();
+    });
+});
